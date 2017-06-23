e360c626f9611ae6594d52831177663ff0fc1fb8
helenypan <yue.pan@me.com>
2017-06-23 11:06:16 +0100

    update djangular settings and urls

--- djangular/settings.py
+++ djangular/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'scrumboard'
+    'scrumboard',
+    'auth_api'
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'djangular.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# why should we set this to let django know about the static files?
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
